752061cabb85a2ac8ac83aec4efbad05376f2a0c
Gavriel770U <kraserultra@gmail.com>
2024-01-21 09:20:12 +0200

    Add GNode and GQueue class skeletons

---
+++ src/bintreevisual/GNode.java
@@ -0,0 +1,17 @@
+package bintreevisual;
+
+// Node that is used for GQueue
+
+public class GNode <T>
+{
+    public GNode()
+    {
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "GNode";
+    }
+}
---
+++ src/bintreevisual/GQueue.java
@@ -0,0 +1,17 @@
+package bintreevisual;
+
+// Queue for Breadth First algorithm
+
+public class GQueue <T>
+{
+    public GQueue()
+    {
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Queue";
+    }
+}
