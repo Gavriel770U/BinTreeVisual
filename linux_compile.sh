7e54b2e49c3eb6e5382e79f36dfc160d02503211
Gavriel Linoy <kraserultra@gmail.com>
2024-02-03 21:25:24 +0200

    Fix linux compile and run #1

--- linux_compile.sh
+++ linux_compile.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 @echo off
 
 set SOURCE_DIR=src
--- src/examples/example_one/linux_compile_example_one.sh
+++ src/examples/example_one/linux_compile_example_one.sh
@@ -1,14 +1,14 @@
-@echo off
+#!/bin/bash
 
-set SOURCE_DIR=../../../src
-set BUILD_DIR=examples_build
+SOURCE_DIR=../../../src
+BUILD_DIR=examples_build
 
-cd %SOURCE_DIR%
-javac -d ../%BUILD_DIR% ./bintreevisual/*.java
-javac -d ../%BUILD_DIR% ./examples/example_one/*.java
+cd "$SOURCE_DIR"
+javac -d ../"$BUILD_DIR" ./bintreevisual/*.java
+javac -d ../"$BUILD_DIR" ./examples/example_one/*.java
 
-cd ../%BUILD_DIR%
+cd ../"$BUILD_DIR"
 jar cfm ExampleOne.jar ./EXAMPLE_ONE_MANIFEST.MF -C bintreevisual/ .
 jar uf ExampleOne.jar -C examples/example_one/ .
 
-echo Build completed successfully.
+echo "Build completed successfully."
